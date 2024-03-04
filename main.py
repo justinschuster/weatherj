7e77d202b2c6cdee84fc3931760f992f68892d0b
justinschuster <schujustin@gmail.com>
2024-03-04 16:00:13 -0500

    Have working api request call

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import requests
 
-url = 'https://api.weather.gov/points/39,-74'
+def get_weather(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        write_json(response.text)
+        data = response.json()
+    else:
+        print("Error", response.status_code)  
 
-response = requests.get(url)
+def write_json(data):
+    file = open('test_data/example.json', 'w')
+    file.write(data)
+    file.close()
 
-if response.status_code == 200:
-    data = response.json()
-    print(data['properties']) 
-else:
-    print("Error", response.status_code)
+if __name__=='__main__':
+    long = 39
+    lat = -74
+    url = 'https://api.weather.gov/points/{},{}'.format(long, lat)
+    get_weather(url)
